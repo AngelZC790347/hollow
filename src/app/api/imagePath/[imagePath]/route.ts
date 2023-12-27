41d2cca2180272de83f75abb7daea5af5356a822
angelzuniga <angel1200z@hotmail.com>
2023-12-26 20:33:28 -0500

    fix request

--- src/app/api/imagePath/[imagePath]/route.ts
+++ src/app/api/imagePath/[imagePath]/route.ts
@@ -2,7 +2,7 @@ const baseURL = "https://api.themoviedb.org/3/"
 const authHeader = {
     "Authorization":`Bearer ${process.env.API_KEY}`
 }
-export async function GET({params}:{params:{imagePath:string}}) {
+export async function GET(req:Request,{params}:{params:{imagePath:string}}) {
     console.log(baseURL+"movie/872585/images/"+params.imagePath)
     const stream = await (fetch("https://image.tmdb.org/t/p/original/"+params.imagePath,{        
         headers:authHeader,    
